8f487a0d7408a5d69ccd7b14dd09003a512b0701
youjin <youjin@youjinui-MacBookPro.local>
2024-08-06 19:10:39 +0900

    test

---
+++ src/component/userfile.js
@@ -0,0 +1,32 @@
+import React, { Component, createRef } from 'react';
+
+class Userfile extends Component {
+    constructor(props) { }
+
+    componentDidUpdate(prevProps) {
+
+    }
+
+    render() {
+        return (
+            <div>
+                {/* <DropdownButton id="dropdown-basic-button" title="실험자">
+              <Dropdown.Item onClick={() => this.handleChange({ target: { value: '단순한' } }, 'pitch_value')}>p1</Dropdown.Item>
+              <Dropdown.Item onClick={() => this.handleChange({ target: { value: '복잡한' } }, 'pitch_value')}>p2</Dropdown.Item>
+              <Dropdown.Item onClick={() => this.handleChange({ target: { value: '고조되는' } }, 'pitch_value')}>p3</Dropdown.Item>
+              <Dropdown.Item onClick={() => this.handleChange({ target: { value: '저하되는' } }, 'pitch_value')}>p4</Dropdown.Item>
+              <Dropdown.Item onClick={() => this.handleChange({ target: { value: '반복적인' } }, 'pitch_value')}>p5</Dropdown.Item>
+              <Dropdown.Item onClick={() => this.handleChange({ target: { value: '변화무쌍한' } }, 'pitch_value')}>p6</Dropdown.Item>
+              <Dropdown.Item onClick={() => this.handleChange({ target: { value: '저하되는' } }, 'pitch_value')}>p7</Dropdown.Item>
+              <Dropdown.Item onClick={() => this.handleChange({ target: { value: '반복적인' } }, 'pitch_value')}>p8</Dropdown.Item>
+              <Dropdown.Item onClick={() => this.handleChange({ target: { value: '변화무쌍한' } }, 'pitch_value')}>p9</Dropdown.Item>
+            </DropdownButton> */}
+                {/* <button onClick={handleButtonClick}>{buttonText}</button> */}
+
+            </div>
+        );
+    }
+
+}
+
+export default Userfile;
