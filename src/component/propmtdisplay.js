6284a0ca03946daba96161d640b6a16c3a7ca90f
moon920110 <moon920110@gmail.com>
2024-08-09 14:18:34 +0900

    Add .idea to gitignore and printout metacode lists to the prompt

--- src/component/propmtdisplay.js
+++ src/component/propmtdisplay.js
@@ -7,6 +7,7 @@ const PromptDisplay = ({ prompt, setPrompt, changedata, totaldata }) => {
     let instrumentsPrompt = '';
     let emotionPrompt = '';
     let pitchPrompt = '';
+    let metaCodes = [];
 
     useEffect(() => {
         setPrompt('')
@@ -26,9 +27,11 @@ const PromptDisplay = ({ prompt, setPrompt, changedata, totaldata }) => {
 
             for (let i = 0; i < pitchChanges.length; i++) {
                 pitchPrompt += `${pitchChanges[i].start}에서 ${pitchChanges[i].end}구간의 음정을 ${pitchChanges[i].meta_tag} 멜로디로, `;
+                if (!metaCodes.includes(pitchChanges[i].meta_tag)) {
+                    metaCodes.push(pitchChanges[i].meta_tag);
+                }
             }
             pitchPrompt += '재생성해줘';
-
         }
 
         if (emotions!=undefined && emotions.length !== 0) {
@@ -36,25 +39,28 @@ const PromptDisplay = ({ prompt, setPrompt, changedata, totaldata }) => {
 
             for (let i = 0; i < emotions.length; i++) {
                 emotionPrompt += `${emotions[i].start}에서 ${emotions[i].end}구간의 감정을 ${emotions[i].emotions} 느낌으로, `;
+                if (!metaCodes.includes(emotions[i].emotions)) {
+                    metaCodes.push(emotions[i].emotions);
+                }
             }
             emotionPrompt += '재생성해줘';
-
         }
 
 
         if (newBeats!=undefined && newBeats.length !== 0 && originalBeats !== undefined) {
             const beatRatio = newBeats.length / originalBeats.length;
             beatPrompt = `주어진 음악의 시간을 ${beatRatio}배로 늘려줘`;
-
         }
 
         if (newinstruments!=undefined && newinstruments.length !== 0 && origininstruments !== undefined) {
             const uniqueinstruments = newinstruments.filter(instrument => !origininstruments.includes(instrument));
             instrumentsPrompt = `주어진 음악에 ${uniqueinstruments} 악기를 추가해줘`;
-
         }
 
-        setPrompt(`${pitchPrompt}\n\n${emotionPrompt}\n\n${beatPrompt}\n\n${instrumentsPrompt}`);
+        // make metaCodes list to string
+        const metaCodesText = `Suno API 메타코드 리스트: ${metaCodes.join(', ')}`;
+
+        setPrompt(`${pitchPrompt}\n\n${emotionPrompt}\n\n${beatPrompt}\n\n${instrumentsPrompt}\n\n${metaCodesText}`);
 
 
         console.log(pitchPrompt, emotionPrompt, beatPrompt, instrumentsPrompt)
